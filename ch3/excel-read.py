50b03d1c117d326947510f7a5df10997c128e6ed
Tattsum <kurohari35@gmail.com>
2017-02-16 19:22:02 +0900

    excelファイルを読む

---
+++ ch3/excel-read.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('/usr/local/lib/python3.5/dist-packages')
+
+import openpyxl
+
+filename = "population.xlsx"
+book = openpyxl.load_workbook(filename)
+
+sheet = book.worksheets[0]
+
+data = []
+
+for row in sheet.rows:
+    data.append([
+        row[0].value,
+        row[2].value
+    ])
+
+del data[0]
+
+data = sorted(data, key=lambda x:x[1])
