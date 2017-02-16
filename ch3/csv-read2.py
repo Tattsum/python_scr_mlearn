07ae533c4f866083d43e25f1e6addfc35ec30559
Tattsum <kurohari35@gmail.com>
2017-02-16 18:56:08 +0900

    csvモジュールを使う

---
+++ ch3/csv-read2.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('/usr/local/lib/python3.5/dist-packages')
+
+import csv, codecs
+
+filename = "list-sjis.csv"
+fp = codecs.open(filename,"r","shift_jis")
+
+reader = csv.reader(fp, delimiter=",", qutechar='""')
+for cells in reader:
+    print(cells[1],cel1[2])
