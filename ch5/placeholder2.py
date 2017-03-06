366d48790c37d52a15f7de9223e668de065061cd
Tattsum <kurohari35@gmail.com>
2017-03-06 15:51:52 +0900

    プレースホルダ2

---
+++ ch5/placeholder2.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('/usr/local/lib/python3.5/dist-packages')
+
+import tensorflow as tf
+
+# プレースホルダを定義 --- (※1)
+a = tf.placeholder(tf.int32, [None]) # 整数型の配列サイズ指定なし
+
+# 配列を10倍にする演算を定義
+b = tf.constant(10);
+x10_op = a * b;
+
+# セッションを開始
+sess = tf.Session()
+
+# プレースホルダーに値を当てはめて実行 --- (※2)
+r1 = sess.run(x10_op, feed_dict={a: [1,2,3,4,5]})
+print(r1)
+r2 = sess.run(x10_op, feed_dict={a: [10,20]})
+print(r2)
