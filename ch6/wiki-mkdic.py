d17ae01dd64931e51ccebc2bf8a97859be273372
Tattsum <kurohari35@gmail.com>
2017-03-06 16:57:41 +0900

    モデムファイル作成

---
+++ ch6/wiki-mkdic.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('/usr/local/lib/python3.5/dist-packages')
+from gensim.models import word2vec
+data = word2vec.Text8Corpus("wiki_wakati.txt")
+model =  word2vec.Word2Vec(data, size=100)
+model.save("wiki.model")
+print("ok")
